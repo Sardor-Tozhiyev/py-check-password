59a76fbf03327939b2095b748aa4a9ae0734690a
Sardor <sardor.tozhiev1994@gmail.com>
2026-06-01 11:33:12 +0300

    flake8 app fixed

--- app/test_main.py
+++ app/test_main.py
@@ -1,60 +1,57 @@
-import pytest
-
-
 from app.main import check_password
 
 
-def test_valid_password():
+def test_valid_password() -> None:
     assert check_password("Passw@rd1") is True
 
 
-def test_small_password():
+def test_small_password() -> None:
     assert check_password("p@E3") is False
 
 
-def test_lower_password():
+def test_lower_password() -> None:
     assert check_password("qwerty") is False
 
 
-def test_minimum_length_valid():
+def test_minimum_length_valid() -> None:
     assert check_password("Pas$w0rd") is True
 
 
-def test_too_long_password():
+def test_too_long_password() -> None:
     assert check_password("Password@23123421") is False
 
 
-def test_no_digits():
+def test_no_digits() -> None:
     assert check_password("Pass@word") is False
 
 
-def test_no_specials():
+def test_no_specials() -> None:
     assert check_password("Password321") is False
 
 
-def test_no_upper_letter():
+def test_no_upper_letter() -> None:
     assert check_password("pass@word123") is False
 
 
-def test_space_in_password():
+def test_space_in_password() -> None:
     assert check_password("Password@ 321") is False
 
 
-def test_all_special_characters():
+def test_all_special_characters() -> None:
     assert check_password("$@#&!-_") is False
 
 
-def test_only_digits():
+def test_only_digits() -> None:
     assert check_password("12345678") is False
 
 
-def test_only_letters():
+def test_only_letters() -> None:
     assert check_password("Password") is False
 
 
-def test_empty_string():
+def test_empty_string() -> None:
     assert check_password("") is False
 
 
-def test_exactly_7_chars():
+def test_exactly_7_chars() -> None:
     assert check_password("Pa@s1wo") is False
